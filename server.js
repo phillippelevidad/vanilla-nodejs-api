b101360639b1d84db3d208fc3a325365e7e4e67a
Phillippe Santana <phillippelevidad@gmail.com>
2022-07-14 11:43:51 -0300

    Added basic routing

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const http = require("http");
 const products = require("./data/products.json");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/json" });
-  res.end(JSON.stringify(products));
+  if (req.url === "/api/products") {
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/json" });
+    res.end(JSON.stringify({ message: "Not found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
