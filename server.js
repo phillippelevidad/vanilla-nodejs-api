84d08861239b58f0de636ba56a2e06d1ce9e05d5
Phillippe Santana <phillippelevidad@gmail.com>
2022-07-14 11:42:35 -0300

    Answering with JSON data

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require("http");
+const products = require("./data/products.json");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World</h1>");
-  res.end();
+  res.writeHead(200, { "Content-Type": "text/json" });
+  res.end(JSON.stringify(products));
 });
 
 const PORT = process.env.PORT || 5000;
