a2530d92cd6d440054f8047610801f9c2e972e32
Phillippe Santana <phillippelevidad@gmail.com>
2022-07-14 11:37:20 -0300

    Responding with http 200 and hello world message

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
 
 const PORT = process.env.PORT || 5000;
 
